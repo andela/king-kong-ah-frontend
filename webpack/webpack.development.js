7707167341b56995bc09b8a51f506ba40b04df59
Emmanuel Oluyale <xyemmanuel@gmail.com>
2019-07-03 18:37:46 +0100

    chore(webpack): add error overlay plugin
    - install error-overlay-webpack-plugin
    - add plugin to webpack.development.js
    
    [Chores #167073294]

--- webpack/webpack.development.js
+++ webpack/webpack.development.js
@@ -1,7 +1,10 @@
+const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
+
 module.exports = {
   devServer: {
     historyApiFallback: true,
     port: 8000,
     publicPath: '/',
   },
+  plugins: [new ErrorOverlayPlugin()],
 };
